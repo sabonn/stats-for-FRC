1110bc48c2206c4b3d6620cdccfce9ba1f2b10cd
sabonn <itay.eshkar@gmail.com>
2023-03-26 19:07:09 +0300

    adding a db(the json file) and getting data for the db

--- lib.py
+++ lib.py
@@ -1,61 +1,95 @@
 import requests
+import json
 
 TBA_Auth_Key = "aiQP5YMSkLerl0ibDSmM77GSDr3tVc9fZr9LIw7fL8AknFM1XuMZRgFtlEMttcBf"
 header = {
-	'accept': 'application/json',
-	'X-TBA-Auth-Key': TBA_Auth_Key,
-	'User-Agent': 'scouting app'
+    'accept': 'application/json',
+    'X-TBA-Auth-Key': TBA_Auth_Key,
+    'User-Agent': 'scouting app'
 }
 
+def on_load(event_key):
+    json_data = {
+        'teams': [],
+    }
+
+    teams = get_info(f'https://www.thebluealliance.com/api/v3/event/{event_key}/teams/keys')
+    for team in teams:
+        json_data['teams'].append({
+            'key': team,
+            'auto_T': 0,
+            'auto_M': 0,
+            'auto_B': 0,
+            'auto_Dock': 0,
+            'tele_T': 0,
+            'tele_M': 0,
+            'tele_B': 0,
+            'tele_Dock': 0,
+            })
+
+    with open('data.json', 'w') as data:
+        json.dump(json_data,data)
+
+def update_db(event_key):
+    data = get_info(f'https://www.thebluealliance.com/api/v3/event/{event_key}/matches')
+
+    search_in_tele = ['tele_T', 'tele_M', 'tele_B', 'tele_Dock']
+    search_in_auto = ['auto_T', 'auto_M', 'auto_B', 'auto_Dock']
+    search = ['T', 'M', 'B']
+
+    current_data = get_db()
+
+    for game in data:
+        current_data = search_data('blue', search_in_auto, search, current_data, game)
+        current_data = search_data('red', search_in_auto, search,current_data, game)
+        current_data = search_data('blue', search_in_tele, search,current_data, game)
+        current_data = search_data('red', search_in_tele, search,current_data, game)
+
+
+    with open('data.json', 'w') as json_data:
+        json.dump(current_data, json_data)
+
+def get_db():
+    current_data = {'Error': 'no data from file'}
+    with open('data.json', 'r') as data:
+        current_data = json.load(data)
+    return current_data
+
+def get_db_team(team_key):
+    current_data = {}
+    with open('data.json', 'r') as data:
+        current_data = json.load(data)
+
+    for i, elem in enumerate(current_data['teams']):
+        if elem['key'] == team_key:
+            return i
+
+    return -1
+
 def get_info(url):
-	info = requests.get(url, headers=header)
-	json = info.json()
-	try:
-		json['Error']
-		return {'Error': 'didnt get data'} 
-	except:
-		return json
+    info = requests.get(url, headers=header)
+    json = info.json()
+    try:
+        json['Error']
+        return {'Error': 'didnt get data'}
+    except:
+        return json
 
-def get_team_status_avg(team_number, year):
-	events = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc{team_number}/events/{year}/keys', headers = header)
-	events_json = events.json()
-	try:
-		events_json['Error']
-		return {'Error': 'faild to get data from api'}
-	except:
-		print("got data")
+def search_data(team, search_in_data, search, current_data, data):
+    cone_count = 0
+    cube_count = 0
 
-	avg_data = []
-	for event in events_json:
-		avg_data.append(get_team_status_event(team_number, event))
+    for i in range(3):
+        cone_count = data['score_breakdown'][team]['autoCommunity'][search[i]].count('Cone')
+        cube_count = data['score_breakdown'][team]['autoCommunity'][search[i]].count('Cube')
+        for j in range(3):
+            current_data['teams'][get_db_team(data['alliances'][team]['team_keys'][j])][search_in_data[j]] += cube_count + cone_count
 
-	return avg_data
+            if data['score_breakdown'][team][f'autoChargeStationRobot{j+1}'] == 'Docked':
+                current_data['teams'][get_db_team(data['alliances'][team]['team_keys'][j])]['auto_Dock'] += 1
+            if data['score_breakdown'][team][f'endGameChargeStationRobot{j+1}'] == 'Docked':
+                current_data['teams'][get_db_team(data['alliances'][team]['team_keys'][j])]['tele_Dock'] += 1
 
-
-def get_team_status_event(team_number, event_key):
-	req = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc{team_number}/event/{event_key}/matches', headers=header)
-	matches = req.json()
-	try:
-		matches['Error']
-		return {'Error': 'faild to get data from api'}
-	except:
-		print("got data")
-
-	important_matches = []
-	for match in matches:
-		if f'frc{team_number}' in match['alliances']['blue']['team_keys'] or f'frc{team_number}' in match['alliances']['red']['team_keys']:
-			important_matches.append(match)
-
-	data = {
-		'avg_T': 0,
-		'avg_M': 0,
-		'avg_B': 0,
-		'avg_auto_T': 0,
-		'avg_auto_M': 0,
-		'avg-auto_B': 0,
-		'avg_Dock': 0,
-		'avg_auto_Dock': 0,
-	}
-
+    return current_data
 
 
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import lib as lib
 
-url = "https://www.thebluealliance.com/api/v3/team/frc"
-
 if __name__ == "__main__":
-	team_number = input("enter team number: ")
-	info = lib.get_info(url + team_number)
-	print(info)
-	events = lib.get_team_status_avg(team_number, 2023)
-	print(events)
+	lib.on_load('2023iscmp')
+	lib.update_db('2023iscmp')
